3fc2cb0b187be116b1672ac8985813680a755f8f
Bgaborr <YR3M6A@std.uni-eszterhazy.hu>
2025-05-09 16:45:27 +0200

    Delete Demo1

--- demo1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.blackjack.demo1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    opens org.blackjack.demo1 to javafx.fxml;
-    exports org.blackjack.demo1;
-}
